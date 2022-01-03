7d44f0a8ff4754214b5f6efb791d67019fb7e950
Yusuf Arslanalp <ayusufa42@gmail.com>
2022-01-03 23:50:26 +0300

    feat: if image has no time property uses current time

--- BackEnd/find_cordinate.py
+++ BackEnd/find_cordinate.py
@@ -1,16 +1,20 @@
 import A_GI
 from PIL import Image
+import datetime
 
 def get_date_taken( path ):
-    return Image.open( path )._getexif()[36867]
+    try:
+        return Image.open( path )._getexif()[36867]
+    except:
+        return datetime.datetime.now()
 
 
 
 #returns lat long
 def find_cordinate( image_name ):
-    date_taken = get_date_taken( image_name )
-
-    #print( date_taken )
+    date_taken = get_date_taken( "ReceivedImages/" + image_name )
+    print( date_taken )
+    return { "lat": 40.809, "longi": 29.362 }
 
 
 #find_cordinate( r"C:\Users\yusuf\AndroidStudioProjects\CompassStarTracker\BackEnd\StarTracker\RPI\Sample_images\26_07_-_20_27_08_image6_700.jpg" )
--- BackEnd/server.py
+++ BackEnd/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, flash, redirect
 from flask_restful import Resource, Api
+import find_cordinate as fc
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,41 +16,21 @@ class Image(Resource):
         return {'hello': 'Image'}
 
     def post( self ):
-        print( "HEREEE00000" )
 
-        # check if the post request has the file part
-        print( request.headers )
-        print( request.form.get('image124') )
-        print( request.files )
-        """if 'img.png' not in request.files:
-            print( "HEREEE0.1" )
-            flash('No file part')
-            return redirect(request.url)"""
 
-        print( "HEREEE1111111111111111111111111111111" )
         file = request.files['file_field']
-        print( "HEREEE2222222222222222222222222222222" )
-
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        print( ":::::::" + file.name )
         
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        #if file and allowed_file(file.filename):
-        print( "HEREEE2" )
+
         #filename = secure_filename(file.filename)
         file.save( "ReceivedImages/" + file.filename )
-        """new_image = Image(
-            path= ".",
-            filename="fff.png",
-            ext= "png"
-        )"""
-        # Save new_image model
-        #return redirect(url_for('uploaded_file', filename="fff.png"))
+
+
+
         print( "Before return" )
-        return { "lat": 40.809, "longi": 29.362 }
+        return fc.find_cordinate( file.filename )
 
 
 api.add_resource(HelloWorld, '/')

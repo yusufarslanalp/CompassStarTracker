110f7faf752b5b255896bef8a3ab95b411f2b116
Yusuf Arslanalp <ayusufa42@gmail.com>
2022-01-04 17:58:42 +0300

    feat: implements find_cordinate() function

--- BackEnd/find_cordinate.py
+++ BackEnd/find_cordinate.py
@@ -1,6 +1,11 @@
 import A_GI
 from PIL import Image
 import datetime
+import os
+from pathlib import Path
+import A_BI as A_BI_file
+import numpy as np
+import math as math
 
 def get_date_taken( path ):
     try:
@@ -8,13 +13,50 @@ def get_date_taken( path ):
     except:
         return datetime.datetime.now()
 
+def find_attitude():
+    
+    currentDir = Path( os.getcwd() )
+    dirStarTracker = Path.joinpath( currentDir, "StarTracker" )
+    dirRPI = Path.joinpath( dirStarTracker, "RPI" )
+    print( dirRPI )
 
+
+    os.chdir( str( dirRPI ) )
+    os.system( "ls" )
+
+
+    os.system( "python2 StarTracker_10_deg.py image-2.jpg" )
+    print( "hereeeeeeeeeeeeeeeeeeeee" )
+
+
+    f = open("ProcessCommunication.txt", "r")
+    arr = f.read().split( " " )
+    ra, dec, roll = float( arr[0] ), float( arr[1] ), float( arr[2] )
+
+    #print( f.read().split( " " ) )
+    f.close()    
+    return ra, dec, roll
 
 #returns lat long
 def find_cordinate( image_name ):
     date_taken = get_date_taken( "ReceivedImages/" + image_name )
     print( date_taken )
+
+    A_IG = A_GI.calc_A_IG( date_taken )
+    print( A_IG )
+    ra, dec, roll = find_attitude()
+    A_BI = A_BI_file.find_A_BI( ra, dec, roll )
+
+    A = np.matmul( A_BI , A_IG )
+
+    lat = math.acos( A[2][2] )
+    long = math.atan( A[2][1] / A[2][0] )
+
+    print( "---------------------------------------------" )
+    print( long )
+
+
     return { "lat": 40.809, "longi": 29.362 }
 
 
-#find_cordinate( r"C:\Users\yusuf\AndroidStudioProjects\CompassStarTracker\BackEnd\StarTracker\RPI\Sample_images\26_07_-_20_27_08_image6_700.jpg" )
+find_cordinate( r"C:\Users\yusuf\AndroidStudioProjects\CompassStarTracker\BackEnd\StarTracker\RPI\Sample_images\26_07_-_20_27_08_image6_700.jpg" )
